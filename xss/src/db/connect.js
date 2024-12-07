51867634592a1b4b8d0bc6cd9a033d386c7f3185
Gumn4m1 <98003893+Pankaj0038@users.noreply.github.com>
2024-12-07 15:31:03 +0530

    Delete xss directory

--- xss/src/db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://0.0.0.0:27017/creds").then(()=>{
-	console.log("Connection successfully");
-}).catch((e)=>{
-	console.log(`Error:${e}`);
-})
